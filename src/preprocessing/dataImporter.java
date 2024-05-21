bacb3fe3da463c1fa0322a2f7c26f8d2cd79c33f
Quang-Dieu Nguyen <86088186+itzmealvin@users.noreply.github.com>
2024-05-21 23:26:01 +0700

    feat: add a data importer class

---
+++ src/preprocessing/dataImporter.java
@@ -0,0 +1,25 @@
+package preprocessing;
+
+import weka.core.converters.ConverterUtils.DataSource;
+
+public class dataImporter {
+
+    public static DataSource trainSource;
+    public static DataSource testSource;
+    public static DataSource validSource;
+
+    static {
+
+        try {
+            trainSource = new DataSource("data/family/training_data.arff");
+            testSource = new DataSource("data/family/test_data.arff");
+            validSource = new DataSource("data/family/validation_data.arff");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
+}
